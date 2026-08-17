6be87083dcfd18af59dd05bfdf1a9795a8926baf
_devloper <codezpark1@gmail.com>
2026-08-17 20:59:23 +0530

    add

--- frontend-nextjs/src/pages/catalogue.tsx
+++ frontend-nextjs/src/pages/catalogue.tsx
@@ -431,7 +431,6 @@ function CatalogueCard({ cat, index }: { cat: CatEntry; index: number }) {
           fill
           sizes="(max-width:768px) 100vw,(max-width:1280px) 50vw,33vw"
           className="cat-img object-cover"
-          style={{ opacity: cat.imgOpacity / 100 }}
         />
         {cat.technical && (
           <div
